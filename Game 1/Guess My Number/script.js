240027b29fdbe14adae0efa3c9d6d5589fd7504a
Hossein Shahmohammadi <imhshahdi@gmail.com>
2022-06-09 19:52:28 +0430

    Refactoring Our Code

--- Game 1/Guess My Number/script.js
+++ Game 1/Guess My Number/script.js
@@ -6,7 +6,6 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     //console.log(guess, typeof guess);
